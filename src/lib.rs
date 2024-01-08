6b60601a717692441ac28d4157f5a95e7324ad1b
Kees-Jan Dijkzeul <kees-jan@dijkzeul.eu>
2024-01-08 19:59:26 +0100

    tt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 type PylosResult<T> = Result<T, String>;
 
-fn total_balls_for_layer_count(layers: u32) -> u32 {
+fn total_positions_for_layer_count(layers: u32) -> u32 {
     layers * (layers + 1) * (2 * layers + 1) / 6
 }
 
@@ -10,11 +10,26 @@ struct GameConstants {
 
 impl GameConstants {
     fn build(layers: u32) -> PylosResult<GameConstants> {
-        let total_balls = total_balls_for_layer_count(layers);
+        let total_balls = total_positions_for_layer_count(layers);
         if total_balls > 64 {
             return Err(format!("A game with {layers} layers needs {total_balls} balls, which is more than supported"));
         }
 
         Ok(GameConstants { layers })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::GameConstants;
+
+    #[test]
+    fn can_create_a_four_layer_game() {
+        GameConstants::build(4).unwrap();
+    }
+
+    #[test]
+    fn cant_create_a_ten_layer_game() {
+        assert!(GameConstants::build(10).is_err());
     }
 }
