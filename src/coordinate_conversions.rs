891551f4f28f7765ace4f8502a24f2bc431489f5
Kees-Jan Dijkzeul <kees-jan@dijkzeul.eu>
2024-03-27 19:15:12 +0100

    tt

---
+++ src/coordinate_conversions.rs
@@ -0,0 +1,60 @@
+pub fn total_positions_for_layer_count(layers: u32) -> u32 {
+    layers * (layers + 1) * (2 * layers + 1) / 6
+}
+
+pub fn size_for_layer_0_based(layer: u32) -> u32 {
+    layer + 1
+}
+
+fn bit_offset_for_layer_0_based(layer: u32) -> u32 {
+    total_positions_for_layer_count(layer)
+}
+
+pub fn offset_from_0_based_coordinates(layer: u32, x: u32, y: u32) -> u32 {
+    let offset = bit_offset_for_layer_0_based(layer) + y * size_for_layer_0_based(layer) + x;
+    assert!(offset < 64);
+    offset
+}
+
+pub fn coordinates_0_based_from_offset(offset: u32) -> (u32, u32, u32) {
+    let mut previous = 0u32;
+    let (layer, _, layer_offset) = (0..)
+        .map(|l| {
+            let current = bit_offset_for_layer_0_based(l + 1);
+            let result = (l, current, previous);
+            previous = current;
+            result
+        })
+        .find(|(_, bit_offset, _)| *bit_offset > offset)
+        .unwrap();
+    let remainder = offset - layer_offset;
+    let y = remainder / size_for_layer_0_based(layer);
+    let x = remainder % size_for_layer_0_based(layer);
+    (layer, x, y)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn bit_offset_for_layer() {
+        assert_eq!(super::bit_offset_for_layer_0_based(3), 14);
+        assert_eq!(super::bit_offset_for_layer_0_based(2), 5);
+        assert_eq!(super::bit_offset_for_layer_0_based(1), 1);
+        assert_eq!(super::bit_offset_for_layer_0_based(0), 0);
+    }
+
+    #[test]
+    fn zero_based_positions() {
+        let offset = super::offset_from_0_based_coordinates(2, 1, 0);
+        let (layer, x, y) = super::coordinates_0_based_from_offset(offset);
+        assert_eq!(layer, 2);
+        assert_eq!(x, 1);
+        assert_eq!(y, 0);
+
+        let offset = super::offset_from_0_based_coordinates(3, 3, 3);
+        let (layer, x, y) = super::coordinates_0_based_from_offset(offset);
+        assert_eq!(layer, 3);
+        assert_eq!(x, 3);
+        assert_eq!(y, 3);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,11 @@
-type PylosResult<T> = Result<T, String>;
+use crate::coordinate_conversions::{
+    coordinates_0_based_from_offset, offset_from_0_based_coordinates, size_for_layer_0_based,
+    total_positions_for_layer_count,
+};
 
-fn total_positions_for_layer_count(layers: u32) -> u32 {
-    layers * (layers + 1) * (2 * layers + 1) / 6
-}
+pub type PylosResult<T> = Result<T, String>;
 
-fn size_for_layer_0_based(layer: u32) -> u32 {
-    layer + 1
-}
-
-fn bit_offset_for_layer_0_based(layer: u32) -> u32 {
-    total_positions_for_layer_count(layer)
-}
-
-fn offset_from_0_based_coordinates(layer: u32, x: u32, y: u32) -> u32 {
-    let offset = bit_offset_for_layer_0_based(layer) + y * size_for_layer_0_based(layer) + x;
-    assert!(offset < 64);
-    offset
-}
-
-fn coordinates_0_based_from_offset(offset: u32) -> (u32, u32, u32) {
-    let mut previous = 0u32;
-    let (layer, _, layer_offset) = (0..)
-        .map(|l| {
-            let current = bit_offset_for_layer_0_based(l + 1);
-            let result = (l, current, previous);
-            previous = current;
-            result
-        })
-        .find(|(_, bit_offset, _)| *bit_offset > offset)
-        .unwrap();
-    let remainder = offset - layer_offset;
-    let y = remainder / size_for_layer_0_based(layer);
-    let x = remainder % size_for_layer_0_based(layer);
-    (layer, x, y)
-}
+mod coordinate_conversions;
 
 #[derive(Debug, PartialEq)]
 struct GameConstants {
@@ -111,29 +83,6 @@ impl Board {}
 #[cfg(test)]
 mod tests {
     use crate::{GameConstants, Position};
-
-    #[test]
-    fn bit_offset_for_layer() {
-        assert_eq!(super::bit_offset_for_layer_0_based(3), 14);
-        assert_eq!(super::bit_offset_for_layer_0_based(2), 5);
-        assert_eq!(super::bit_offset_for_layer_0_based(1), 1);
-        assert_eq!(super::bit_offset_for_layer_0_based(0), 0);
-    }
-
-    #[test]
-    fn zero_based_positions() {
-        let offset = super::offset_from_0_based_coordinates(2, 1, 0);
-        let (layer, x, y) = super::coordinates_0_based_from_offset(offset);
-        assert_eq!(layer, 2);
-        assert_eq!(x, 1);
-        assert_eq!(y, 0);
-
-        let offset = super::offset_from_0_based_coordinates(3, 3, 3);
-        let (layer, x, y) = super::coordinates_0_based_from_offset(offset);
-        assert_eq!(layer, 3);
-        assert_eq!(x, 3);
-        assert_eq!(y, 3);
-    }
 
     #[test]
     fn can_create_a_four_layer_game() {
