25de47438657e68f925bb473eb5976e0edab3950
Kees-Jan Dijkzeul <kees-jan@dijkzeul.eu>
2024-03-27 19:06:58 +0100

    tt

--- src/lib.rs
+++ src/lib.rs
@@ -21,15 +21,13 @@ fn offset_from_0_based_coordinates(layer: u32, x: u32, y: u32) -> u32 {
 fn coordinates_0_based_from_offset(offset: u32) -> (u32, u32, u32) {
     let mut previous = 0u32;
     let (layer, _, layer_offset) = (0..)
-        .into_iter()
         .map(|l| {
             let current = bit_offset_for_layer_0_based(l + 1);
             let result = (l, current, previous);
             previous = current;
             result
         })
-        .filter(|(_, bit_offset, _)| *bit_offset > offset)
-        .next()
+        .find(|(_, bit_offset, _)| *bit_offset > offset)
         .unwrap();
     let remainder = offset - layer_offset;
     let y = remainder / size_for_layer_0_based(layer);
@@ -37,6 +35,7 @@ fn coordinates_0_based_from_offset(offset: u32) -> (u32, u32, u32) {
     (layer, x, y)
 }
 
+#[derive(Debug, PartialEq)]
 struct GameConstants {
     layers: u32,
 }
@@ -52,44 +51,48 @@ impl GameConstants {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
 struct Position<'a> {
     gc: &'a GameConstants,
     offset: u32,
 }
 
 impl Position<'_> {
-    // fn build(gc: &GameConstants, layer: u32, x: u32, y: u32) -> PylosResult<Position> {
-    //     if layer > gc.layers {
-    //         return Err(format!(
-    //             "In a game with {} layers, there is no position on layer {}",
-    //             gc.layers, layer
-    //         ));
-    //     }
-    //     if layer == 0 {
-    //         return Err(String::from("Layer 0 does not exist"));
-    //     }
-    //
-    //     let layer_size = gc.size_for_layer(layer - 1);
-    //     if x == 0 || x > layer_size {
-    //         return Err(format!(
-    //             "x-coordinate {} is outside range 1..{}",
-    //             x, layer_size
-    //         ));
-    //     }
-    //     if y == 0 || y > layer_size {
-    //         return Err(format!(
-    //             "y-coordinate {} is outside range 1..{}",
-    //             y, layer_size
-    //         ));
-    //     }
-    //
-    //     Ok(Position::build_0_based(gc, layer - 1, x - 1, y - 1))
-    // }
+    fn build(gc: &GameConstants, layer: u32, x: u32, y: u32) -> PylosResult<Position> {
+        if layer > gc.layers {
+            return Err(format!(
+                "In a game with {} layers, there is no position on layer {}",
+                gc.layers, layer
+            ));
+        }
+        if layer == 0 {
+            return Err(String::from("Layer 0 does not exist"));
+        }
+        let layer_0_based = gc.layers - layer;
+        let layer_size = size_for_layer_0_based(layer_0_based);
+        if x == 0 || x > layer_size {
+            return Err(format!(
+                "x-coordinate {} is outside range 1..{}",
+                x, layer_size
+            ));
+        }
+        if y == 0 || y > layer_size {
+            return Err(format!(
+                "y-coordinate {} is outside range 1..{}",
+                y, layer_size
+            ));
+        }
 
-    // fn coordinates(&self) -> (u32, u32, u32) {
-    //     let (layer, x, y) = self.coordinates_0_based();
-    //     (size_for_layer_0_based(layer), x + 1, y + 1)
-    // }
+        Ok(Position {
+            gc,
+            offset: offset_from_0_based_coordinates(layer_0_based, x - 1, y - 1),
+        })
+    }
+
+    fn coordinates(&self) -> (u32, u32, u32) {
+        let (layer, x, y) = coordinates_0_based_from_offset(self.offset);
+        (self.gc.layers - layer, x + 1, y + 1)
+    }
 }
 
 struct PositionSet {
@@ -142,33 +145,37 @@ mod tests {
         assert!(GameConstants::build(10).is_err());
     }
 
-    // #[test]
-    // fn position_construction_and_getters() {
-    //     let gc = GameConstants::build(3).unwrap();
-    //     let position = Position::build(&gc, 1, 2, 3).unwrap();
-    //     let (layer, x, y) = position.coordinates();
-    //     assert_eq!(layer, 1);
-    //     assert_eq!(x, 2);
-    //     assert_eq!(y, 3);
-    //     let (layer, x, y) = position.coordinates_0_based();
-    //     assert_eq!(layer, 0);
-    //     assert_eq!(x, 1);
-    //     assert_eq!(y, 2);
-    // }
-    // #[test]
-    // fn valid_positions() {
-    //     let gc = GameConstants::build(2).unwrap();
-    //     let positions = [
-    //         Position::build(&gc, 1, 1, 1),
-    //         Position::build(&gc, 1, 1, 2),
-    //         Position::build(&gc, 1, 2, 1),
-    //         Position::build(&gc, 1, 2, 2),
-    //         Position::build(&gc, 2, 1, 1),
-    //     ];
-    // }
-    // #[test]
-    // fn invalid_positions() {
-    //     let gc = GameConstants::build(2).unwrap();
-    //     let positions = [Position::build(&gc, 0, 0, 9)];
-    // }
+    #[test]
+    fn position_construction_and_getters() {
+        let gc = GameConstants::build(3).unwrap();
+        let position = Position::build(&gc, 1, 2, 3).unwrap();
+        let (layer, x, y) = position.coordinates();
+        assert_eq!(layer, 1);
+        assert_eq!(x, 2);
+        assert_eq!(y, 3);
+    }
+    #[test]
+    fn valid_positions() {
+        let gc = GameConstants::build(2).unwrap();
+        let _ = [
+            Position::build(&gc, 1, 1, 1).unwrap(),
+            Position::build(&gc, 1, 1, 2).unwrap(),
+            Position::build(&gc, 1, 2, 1).unwrap(),
+            Position::build(&gc, 1, 2, 2).unwrap(),
+            Position::build(&gc, 2, 1, 1).unwrap(),
+        ];
+    }
+    #[test]
+    fn invalid_positions() {
+        let gc = GameConstants::build(2).unwrap();
+        let positions = [
+            Position::build(&gc, 0, 0, 0),
+            Position::build(&gc, 1, 0, 0),
+            Position::build(&gc, 1, 3, 1),
+            Position::build(&gc, 1, 1, 3),
+            Position::build(&gc, 3, 1, 1),
+        ];
+
+        assert_eq!(None, positions.into_iter().find(|p| p.is_ok()));
+    }
 }
